7f7ea57f0ebf14181f25cbae4a4d31f9a64b7fec
Daniel <danielll20@hotmail.com>
2015-05-20 12:52:33 -0300

    Criação do template e tela de login

---
+++ S001-SistemaEscolarWeb/src/br/com/sistemaescolar/mb/LoginMB.java
@@ -0,0 +1,35 @@
+package br.com.sistemaescolar.mb;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LoginMB {
+
+	private String login;
+	
+	private String senha;
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public String logar() {
+		if(login.equals("admin") && senha.equals("admin")){
+			return "aluno/cadastrar-aluno";
+		}else{		
+			return "login";
+		}
+	}
+}
