38167e3a57513ec06e39409750ad5b7c02c9966c
Daniel Correia <Daniel Correia@DanielCorreia>
2015-06-02 12:29:34 -0300

    Alteração da interface.

--- S001-SistemaEscolarClient/ejbModule/br/com/sistemaescolar/applicationservice/entitycontroller/AlunoECLocal.java
+++ S001-SistemaEscolarClient/ejbModule/br/com/sistemaescolar/applicationservice/entitycontroller/AlunoECLocal.java
@@ -2,14 +2,11 @@ package br.com.sistemaescolar.applicationservice.entitycontroller;
 
 import java.util.List;
 
-import javax.ejb.Local;
+import br.com.sistemaescolar.entidade.Aluno;
 
-import br.com.sistemaescolar.to.AlunoTO;
-
-@Local
 public interface AlunoECLocal {
 
-	public void cadastrar(AlunoTO alunoTO);
+	public void cadastrar(Aluno aluno);
 	
-	public List<AlunoTO> consultarTodosAluno();
+	public List<Aluno> consultarTodosAluno();
 }
