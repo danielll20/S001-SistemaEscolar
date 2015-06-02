4278c795d57e481c1d917f9e8e3ff7f6969e7ffb
Daniel Correia <Daniel Correia@DanielCorreia>
2015-06-02 12:27:52 -0300

    Alteração do tipo de retorno na consulta aluno.

--- S001-SistemaEscolar/ejbModule/br/com/sistemaescolar/fachada/applicationservice/entitycontroller/AlunoEC.java
+++ S001-SistemaEscolar/ejbModule/br/com/sistemaescolar/fachada/applicationservice/entitycontroller/AlunoEC.java
@@ -11,9 +11,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import br.com.sistemaescolar.applicationservice.entitycontroller.AlunoECLocal;
-import br.com.sistemaescolar.converter.ConverteFactory;
 import br.com.sistemaescolar.entidade.Aluno;
-import br.com.sistemaescolar.to.AlunoTO;
 
 /**
  * @author Daniel Correia
@@ -29,18 +27,17 @@ public class AlunoEC implements AlunoECLocal {
 	 * @see br.com.sistemaescolar.applicationservice.entitycontroller.AlunoECLocal#cadastrar(br.com.sistemaescolar.to.AlunoTO)
 	 */
 	@Override
-	public void cadastrar(AlunoTO alunoTO) {
-		Aluno aluno = ConverteFactory.converteAlunoTO(alunoTO);
+	public void cadastrar(Aluno aluno) {
 		entityManager.persist(aluno);
 	}
 
 	@Override
-	public List<AlunoTO> consultarTodosAluno() {		 
+	public List<Aluno> consultarTodosAluno() {		 
 		StringBuilder jpql = new StringBuilder();
 		jpql.append("select a from Aluno a ");	
 		
-		Query query = entityManager.createQuery(jpql.toString());
-		List<AlunoTO> lista = query.getResultList();
+		Query query = entityManager.createQuery(jpql.toString(), Aluno.class);
+		List<Aluno> lista = query.getResultList();
 		
 		return lista;
 	}
